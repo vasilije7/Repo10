2cb3433cc075d7bbebd0ec8288d0d2c6eea3161c
Vasilije Vujovic <vasilijev7@gmail.com>
2022-04-06 15:01:19 +0200

    Dodata vjezba 4 i prepravljena ex01

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,12 +1,12 @@
 // Only change code below this line
 var myPet = {
-name: "Grof",
-species: "Rotvajler",
-legs: 4,
-friends: ["Rik", "Don"]
-}
-function myFunction(myObj){
-return myObj;
+  name: "Grof",
+  species: "Rotvajler",
+  legs: 4,
+  friends: ["Rik", "Don"],
+};
+function myFunction(myObj) {
+  return myObj;
 }
 // Only change code above this line
 console.log(myFunction(myPet.name)); //Change this line
