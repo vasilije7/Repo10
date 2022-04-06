cfa27fefa88176cf3278e7130a4ded328d82fe70
Vasilije Vujovic <vasilijev7@gmail.com>
2022-04-06 13:05:30 +0200

    Vjezba 1

---
+++ ex01/src/index.js
@@ -0,0 +1,13 @@
+// Only change code below this line
+var myPet = {
+name: "Grof",
+species: "Rotvajler",
+legs: 4,
+friends: ["Rik", "Don"]
+}
+function myFunction(myObj){
+return myObj;
+}
+// Only change code above this line
+console.log(myFunction(myPet.name)); //Change this line
+module.exports = { myPet, myFunction };
