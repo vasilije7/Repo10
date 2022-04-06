fa3eef6a3a5a5d71ce4a72b06e8b6a1d671ff3be
Vasilije Vujovic <vasilijev7@gmail.com>
2022-04-06 15:36:13 +0200

    Vjezba 6

---
+++ ex06/src/index.js
@@ -0,0 +1,15 @@
+// Create lion object here
+var lion = {
+  name: "Simba",
+  legs: 4,
+  tails: 1,
+};
+// End of lion object
+// Create a function below this line
+function myFunction(propName, propValue) {
+  lion[propName] = propValue;
+  return lion;
+}
+// End of function
+console.log(myFunction("roar", "roar-roar"));
+module.exports = myFunction;
