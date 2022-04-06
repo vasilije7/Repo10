05bd23501fd2fee0234d12a90af41429159d0f16
Vasilije Vujovic <vasilijev7@gmail.com>
2022-04-06 22:13:47 +0200

    Prepravka

--- ex09/src/index.js
+++ ex09/src/index.js
@@ -2,10 +2,8 @@ function myFunction(myObj, checkProp) {
   //Only change code below this line
   if (myObj.hasOwnProperty(checkProp)) {
     return myObj[checkProp];
-  } else {
-    return "Not found";
   }
-  return "Change me!";
+  return "Not found";
   //Only change code above this line
 }
 console.log(
